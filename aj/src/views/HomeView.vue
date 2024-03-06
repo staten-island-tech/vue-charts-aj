5141c8394f9c3135017317d20f084da9adb85a52
azheng606 <144460945+azheng606@users.noreply.github.com>
2024-03-06 10:16:57 -0500

    a

--- aj/src/views/HomeView.vue
+++ aj/src/views/HomeView.vue
@@ -1,5 +1,9 @@
 <script setup>
 import TheWelcome from '../components/TheWelcome.vue'
+import { createApp } from 'vue'
+import App from '../components/TheWelcome.vue'
+
+createApp(App).mount('#app')
 </script>
 
 <template>
