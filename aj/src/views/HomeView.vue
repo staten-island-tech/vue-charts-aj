1177aa85eac8d894370b6f5ee6b69e848eed1374
azheng606 <144460945+azheng606@users.noreply.github.com>
2024-03-08 10:14:40 -0500

    ,l

--- aj/src/views/HomeView.vue
+++ aj/src/views/HomeView.vue
@@ -1,3 +1,10 @@
+<!-- 
+<template>
+  <main>
+    <TheWelcome />
+  </main>
+</template>
+
 <script setup>
 import TheWelcome from '../components/TheWelcome.vue'
 import { createApp } from 'vue'
@@ -5,9 +12,29 @@ import App from '../components/TheWelcome.vue'
 
 createApp(App).mount('#app')
 </script>
+ -->
 
-<template>
-  <main>
-    <TheWelcome />
-  </main>
+ <template>
+  <div class=" title"> THINGS </div>
+  <DestCard v-for="poke in pokemon"
+  :key = "poke.name">
+  </DestCard>
 </template>
+
+<script setup>
+import {ref, onBeforeMounted} from 'vue'
+const pokemon = ref('')
+async function getPokemon () { 
+  let rest = await fetch ("https://data.cityofnewyork.us/resource/pasr-j7fb.json")
+  let data = await rest.json();
+  pokemon.value = data;
+}
+onBeforeMounted(() => {
+  getPokemon();
+})
+import DestCard from "@/components/DestCard.vue;"
+</script>
+
+<style scoped>
+
+</style> 
