ac720e6ce55c40d8bfbfa322e1d674cfa7fb5cb7
juuclue <144699439+juuclue@users.noreply.github.com>
2024-03-12 10:21:04 -0400

    e

--- aj/src/views/AboutView.vue
+++ aj/src/views/AboutView.vue
@@ -1,18 +1,9 @@
 <template>
   <div>
-<h1></h1>
+<h1>hi</h1>
   </div>
 </template>
 
 <script setup>
-/* import { ref, onBeforeMounted} from 'vue'
-const edu = ref('')
-async function  getEdu (){
-  let rest = await fetch ("https://data.cityofnewyork.us/resource/43nn-pn8j.json")
-  let data = await rest.json();
-  edu.value = data;
-}
-onBeforeMounted(() => {
- getEdu();
-}) */
+
 </script>
--- aj/src/views/BarChart.vue
+++ aj/src/views/BarChart.vue
@@ -1,55 +1,23 @@
-<!-- <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
-</script>
-
 <template>
   <main>
-    <TheWelcome />
     <h1>BAR CHART</h1>
   </main>
 </template> 
-<script>
-import { bar } from 'vue-chart.js'
-import { ref, onBeforeMounted} from 'vue'
+<script setup>
+
+import { ref, onBeforeMount} from 'vue'
 const edu = ref('')
-async function  getEdu (){
-  let rest = await fetch ("https://data.cityofnewyork.us/resource/43nn-pn8j.json")
+async function getEdu (){
+  let rest = await fetch("https://data.cityofnewyork.us/resource/pasr-j7fb.json")
   let data = await rest.json();
   edu.value = data;
+  console.log(data)
 }
-onBeforeMounted(() => {
+
+onBeforeMount(() => {
  getEdu();
 }) 
 
-import
-</script> -->
-<template>
-  <Bar
-    id="my-chart-id"
-    :options="chartOptions"
-    :data="chartData"
-  />
-</template>
-
-<script>
-import { Bar } from 'vue-chart.js'
-import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
-
-ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
-
-export default {
-  name: 'BarChart',
-  components: { Bar },
-  data() {
-    return {
-      chartData: {
-        labels: [ 'January', 'February', 'March' ],
-        datasets: [ { data: [40, 20, 12] } ]
-      },
-      chartOptions: {
-        responsive: true
-      }
-    }
-  }
-}
+import HelloWorld from '@/components/HelloWorld.vue';
 </script>
+
