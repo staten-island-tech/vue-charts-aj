ca20aa1a9acade167e8f11ca6a082bb7df995485
azheng606 <144460945+azheng606@users.noreply.github.com>
2024-03-14 09:44:22 -0400

    t

--- aj/src/components/HelloWorld.vue
+++ aj/src/components/HelloWorld.vue
@@ -1,11 +1,44 @@
+<script setup>
+defineProps({
+  msg: {
+    type: String,
+    required: true
+  }
+})
+</script>
 
 <template>
-  <h1>hello</h1>
-  <div class="cards">
-    <h2>borough: {{ edu.borough }}</h2>
+  <div class="greetings">
+    <h1 class="green">{{ msg }}</h1>
+    <h3>
+      You’ve successfully created a project with
+      <a href="https://vitejs.dev/" target="_blank" rel="noopener">Vite</a> +
+      <a href="https://vuejs.org/" target="_blank" rel="noopener">Vue 3</a>.
+    </h3>
   </div>
 </template>
 
-<script setup>
+<style scoped>
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  position: relative;
+  top: -10px;
+}
 
-</script>
+h3 {
+  font-size: 1.2rem;
+}
+
+.greetings h1,
+.greetings h3 {
+  text-align: center;
+}
+
+@media (min-width: 1024px) {
+  .greetings h1,
+  .greetings h3 {
+    text-align: left;
+  }
+}
+</style>
