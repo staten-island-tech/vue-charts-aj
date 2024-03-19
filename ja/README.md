6e3e6705da049060d1eeae3ed1de49e049af4033
azheng606 <144460945+azheng606@users.noreply.github.com>
2024-03-19 10:08:21 -0400

    dx

--- ja/README.md
+++ ja/README.md
@@ -1,4 +1,4 @@
-# present
+# ja
 
 This template should help get you started developing with Vue 3 in Vite.
 
---
+++ ja/src/components/BarChart.vue
@@ -0,0 +1,29 @@
+
+<!-- <template>
+  <div>
+<:chartData = "chartData" />
+  </div>
+</template>
+ -->
+<!-- 
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style> -->
+<template>
+  <div>
+    <h2>Bar Chart</h2>
+    <Bar ></Bar>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+
+</style>
