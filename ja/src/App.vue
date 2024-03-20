4dfea127666edb3fa78cbd16622dd17c14a07fff
azheng606 <144460945+azheng606@users.noreply.github.com>
2024-03-20 10:16:25 -0400

    d

--- ja/src/App.vue
+++ ja/src/App.vue
@@ -5,10 +5,7 @@ import { RouterLink, RouterView } from 'vue-router'
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
     <div class="wrapper">
-      <HelloWorld msg="You did it!" />
 
       <nav>
         <RouterLink to="/">Home</RouterLink>
@@ -27,24 +24,11 @@ header {
   max-height: 100vh;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
 nav {
   width: 100%;
   font-size: 12px;
   text-align: center;
   margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
 }
 
 nav a {
@@ -64,9 +48,6 @@ nav a:first-of-type {
     padding-right: calc(var(--section-gap) / 2);
   }
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
 
   header .wrapper {
     display: flex;
--- ja/src/views/BarView.vue
+++ ja/src/views/BarView.vue
@@ -1,20 +1,35 @@
 
+<script>
+import { Bar } from 'vue-chartjs'
+import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
+ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
+
+export default {
+  name: 'BarChart',
+  components: { Bar },
+  data: () => ({
+    loaded: false,
+    chartData: null
+  }),
+  async mounted () {
+    this.loaded = false
+
+    try {
+      const { userlist } = await fetch('/api/userlist')
+      this.chartdata = userlist
+
+      this.loaded = true
+    } catch (e) {
+      console.error(e)
+    }
+  }
+}
+</script>
+
 <template>
-  <div>
-    <h1> Bar Chart</h1>
-<BarChart :poke = "poke"> </BarChart>
+  <div class="container">
+    <Bar v-if="loaded" :data="chartData" />
   </div>
 </template>
 
-<script setup>
-import {defineProps} from 'vue';
-import BarChart from "@/components/BarChart.vue";
-const props = defineProps({
-  poke: Object
-});
-</script>
-
-<style scoped>
-
-</style>
- 
+<style scoped> </style>
